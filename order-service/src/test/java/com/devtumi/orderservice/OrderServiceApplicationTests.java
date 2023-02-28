d18847c224aa9dc29f034f6e4cc4e2d196503c92
B-Tshehla <boitumelotshehla@gmail.com>
2023-02-28 13:18:26 +0200

    init

---
+++ order-service/src/test/java/com/devtumi/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devtumi.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
